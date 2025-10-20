886fd8b4b95bab004d7c9de438f101f142b67173
Jesus Rodriguez <Jesus.Rodriguez@techconsulting.tech>
2025-10-20 13:14:07 -0400

    Activated Welcome message

--- app/components/main/WelcomeMessage.tsx
+++ app/components/main/WelcomeMessage.tsx
@@ -5,32 +5,32 @@ const WelcomeMessage = () => {
   const [customMessage, setCustomMessage] = useState<string>("hey");
   const hasFetched = useRef(false);
 
-  // useEffect(() => {
-  //   // only fetch the welcome message once
-  //   if (hasFetched.current) return;
-  //   hasFetched.current = true;
-  //   const fetchWelcomeMessage = async () => {
-  //     try {
-  //       const response = await fetch("/api/welcome", {
-  //         method: "POST",
-  //         body: JSON.stringify({
-  //           prompt: "Give me a really short Welcome message",
-  //         }),
-  //       });
+  useEffect(() => {
+    // only fetch the welcome message once
+    if (hasFetched.current) return;
+    hasFetched.current = true;
+    const fetchWelcomeMessage = async () => {
+      try {
+        const response = await fetch("/api/welcome", {
+          method: "POST",
+          body: JSON.stringify({
+            prompt: "Give me a really short Welcome message",
+          }),
+        });
 
-  //       const result = await response.json();
-  //       setCustomMessage(result);
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  //   };
+        const result = await response.json();
+        setCustomMessage(result);
+      } catch (e) {
+        console.log(e);
+      }
+    };
 
-  //   fetchWelcomeMessage();
-  // }, []);
+    fetchWelcomeMessage();
+  }, []);
 
   return (
     <div className="flex flex-2 flex-col  mt-35 animate-fadeInUp">
-      <h1 className="text-text text-center ">Hello Jesus!</h1>
+      <h1 className="text-text text-center ">Hello Matt!</h1>
       {customMessage ? (
         <h3 className="text-text-muted animate-fadeInUp text-center">
           {customMessage}
