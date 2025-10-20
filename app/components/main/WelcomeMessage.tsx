82900493154ae50f7775ce9c78413ac1b3312a20
Jesus Rodriguez <Jesus.Rodriguez@techconsulting.tech>
2025-10-20 10:09:51 -0400

    Added api endpoint for welcome message and made implementation on WelcomeMessage component

--- app/components/main/WelcomeMessage.tsx
+++ app/components/main/WelcomeMessage.tsx
@@ -1,12 +1,49 @@
-import React from "react";
+import { useEffect, useState, useRef } from "react";
+import Loading from "../general/Loading";
 
 const WelcomeMessage = () => {
+  const [customMessage, setCustomMessage] = useState<string>("");
+  const hasFetched = useRef(false);
+
+  useEffect(() => {
+    // only fetch the welcome message once
+    if (hasFetched.current) return;
+    hasFetched.current = true;
+    const fetchWelcomeMessage = async () => {
+      try {
+        const response = await fetch("/api/welcome", {
+          method: "POST",
+          body: JSON.stringify({
+            prompt: "Give me a really short Welcome message",
+          }),
+        });
+
+        const result = await response.json();
+
+        console.log("result: ", result);
+
+        setCustomMessage(result);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    fetchWelcomeMessage();
+  }, []);
+
+  console.log("rendered");
   return (
-    <div className="flex flex-2 flex-col items-center mt-35 animate-fadeInUp">
-      <h1 className="text-text ">Hello Jesus!</h1>
-      <h3 className="text-text-muted">
-        Ai Generated Subtitle, changed every time
-      </h3>
+    <div className="flex flex-2 flex-col  mt-35 animate-fadeInUp">
+      <h1 className="text-text text-center ">Hello Jesus!</h1>
+      {customMessage ? (
+        <h3 className="text-text-muted animate-fadeInUp text-center">
+          {customMessage}
+        </h3>
+      ) : (
+        <div className="flex items-center justify-center">
+          <Loading />
+        </div>
+      )}
     </div>
   );
 };
